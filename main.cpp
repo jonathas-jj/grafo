deca10c5ab854fa47c121eaa03b7cbda41943e9e
jonathas-jj <jonathas.quarteroli@gmail.com>
2019-04-18 13:32:48 -0300

    Adicionado a biblioteca json para manipular grafos em arquivos json.

--- main.cpp
+++ main.cpp
@@ -13,8 +13,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include <jsoncpp/json/json.h> 
+#include <jsoncpp/json/json.h>  //biblioteca importada para manipulação de arquivos json.
 #include "Grafo.h"
+#include <fstream>
 using namespace std;
 
 /*
@@ -23,15 +24,23 @@ using namespace std;
 
 int main() {
     Grafo *g1 = new Grafo(grafoMatriz);// grafo do tipo matriz
-    Json::Value val;
     Grafo *g2 = new Grafo(grafoLista);// grafo do tipo lista
     int n=0, v1=0,v2=0; // numero de vertices . Vértice 1 e vértice 2 da aresta
+    fstream arq("dados/grafoN5.json");
+    Json::Value raiz;
+    arq >> raiz;
+    
+    cout << raiz["nome"] << endl;
+    
     
     
     cout << "digite o numero de vértices " << endl ;
     cin >> n;
     g1->criaGrafo(n,0,NULL); // cria grafo com 0 arestas
+   
+    //Lê dois vertíces (v1 e v2: não testa se eles existem) e insere uma aresta no grafo.
     cout << "digite as arestas separadas por espaços.Quando acabar, ou se não houver aresta, digite -1 ." << endl ;
+   
     cin >> v1 ;
     while(v1!=-1){
         cin >> v2;
